c790292791575cd5c119fefc128f917fab6c883a
Navid <navidghate@hotmail.com>
2023-04-04 15:07:17 +0200

    removed period from database and replaced it with a link to the syllabus

--- install.php
+++ install.php
@@ -14,21 +14,20 @@ $sql .= "CREATE TABLE courses(
     courseid VARCHAR(6) PRIMARY KEY NOT NULL,
     courname VARCHAR(20) NOT NULL, 
     progression VARCHAR(1) NOT NULL,
-    period INT(1) NOT NULL
+    syllabus VARCHAR(250) NOT NULL
     );
     ";
 
 
 $sql .= "INSERT INTO courses VALUES
-    ('DT057G', 'Webbutveckling', 'A',1),
-    ('DT087G', 'Introduktion till programmering i JavaScript', 'A', 1),
-    ('DT200G', 'Grafisk teknik för webb', 'A', 2),
-    ('DT068G', 'Webbanvändbarhet', 'B', 2),
-    ('DT003G', 'Databaser', 'A', 3),
-    ('DT093G', 'Frontend-baserad webbutveckling', 'B', 3),
-    ('DT197G', 'Backend-baserad webbutveckling', 'B', 4),
-    ('DT173G', 'Programmering i TypeScript', 'B', 4
-    
+    ('DT057G', 'Webbutveckling', 'A', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/search/?name=&code=dt057g&level=-1&department=-1&faculty=-1&term=-1&version=1&sort=KursBenamning'),
+    ('DT087G', 'Introduktion till programmering i JavaScript', 'A', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/search/?name=&code=dt084g&level=-1&department=-1&faculty=-1&term=-1&version=1&sort=KursBenamning'),
+    ('DT200G', 'Grafisk teknik för webb', 'A', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/search/?name=&code=dt200g&level=-1&department=-1&faculty=-1&term=-1&version=1&sort=KursBenamning'),
+    ('DT068G', 'Webbanvändbarhet', 'B', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/search/?name=&code=dt068g&level=-1&department=-1&faculty=-1&term=-1&version=1&sort=KursBenamning'),
+    ('DT003G', 'Databaser', 'A', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/search/?name=&code=dt003g&level=-1&department=-1&faculty=-1&term=-1&version=1&sort=KursBenamning'),
+    ('DT093G', 'Frontend-baserad webbutveckling', 'B', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/search/?name=&code=dt211g&level=-1&department=-1&faculty=-1&term=-1&version=1&sort=KursBenamning'),
+    ('DT197G', 'Backend-baserad webbutveckling', 'B', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/search/?name=&code=dt207g&level=-1&department=-1&faculty=-1&term=-1&version=1&sort=KursBenamning'),
+    ('DT173G', 'Programmering i TypeScript', 'B', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/search/?name=&code=dt208g&level=-1&department=-1&faculty=-1&term=-1&version=1&sort=KursBenamning'
     );
     ";
 
