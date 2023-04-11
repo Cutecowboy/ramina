6f0bda0998e606fa9ed913c52102a775f99f8d45
Cutecowboy <110782320+Cutecowboy@users.noreply.github.com>
2023-04-11 11:30:31 +0200

    Update config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php 
+
+spl_autoload_register(function($class_name){
+    include 'classes/' . $class_name . '.class.php';
+});
+
+$devmode = true;
+
+if($devmode) {
+    error_reporting(-1);
+    ini_set("display_errors", 1);
+
+    define("DBHOST", "localhost");
+    define("DBUSER", "courseapi");
+    define("DBPASS", "password");
+    define("DBDATABASE", "courseapi");
+} else {
+    // lägg in dbserver i molnet
+    define("DBHOST", "localhost");
+    define("DBUSER", "courseapi");
+    define("DBPASS", "password");
+    define("DBDATABASE", "courseapi");
+}
+
